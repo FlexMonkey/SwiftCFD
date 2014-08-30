1d9cc422f38fac7bce09d4a5f11480b44f69a43d
Simon Gladman <simongladman@Simons-MacBook-Pro.local>
2014-08-30 06:24:28 +0100

    WIP

---
+++ FluidDynamics/FluidDynamicsCell.swift
@@ -0,0 +1,23 @@
+//
+//  FluidDynamicsCell.swift
+//  FluidDynamics
+//
+//  Created by Simon Gladman on 27/08/2014.
+//  Copyright (c) 2014 Simon Gladman. All rights reserved.
+//
+
+import Foundation
+
+struct FluidDynamicsCell
+{
+    var density : Double = 0;
+    var u : Double = 0;
+    var v : Double = 0;
+    var curl : Double = 0;
+    
+    mutating func setUV(value:(u : Double, v : Double))
+    {
+        self.u = value.u;
+        self.v = value.v;
+    }
+}
